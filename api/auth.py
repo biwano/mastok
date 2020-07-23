920fcd105a322578e29d04e0f67347805bf5597d
bilponse <bilponse@localhost.localdomain>
2020-07-23 22:09:52 +0200

    Authentication

--- api/auth.py
+++ api/auth.py
@@ -11,7 +11,7 @@ import config
 
 def user_with_key(user, api_key):
     res = user.to_dict()
-    res["api_key"] = api_key.api_key
+    res["api_key"] = api_key
     return res
 
 def set_user_passcode(session, mail):
@@ -28,10 +28,10 @@ def auth_by_mail(session: helpers.extend.session, response, mail, passcode=None,
     try:
         query = session.query(User).filter(User.mail == mail)
         requested_user = query.one()
-        new_key = ApiKey(user=requested_user, api_key=helpers.make_key())
+        new_key = helpers.make_key()
         # Standard api_key authorization
         if api_key:
-            if api_key in requested_user.api_keys:
+            if list(filter(lambda x: x.api_key==api_key, requested_user.api_keys)):
                 return user_with_key(requested_user, api_key)
             return helpers.response.error("API key authentication_failed", falcon.HTTP_401)
         # Passcode verification
@@ -41,20 +41,22 @@ def auth_by_mail(session: helpers.extend.session, response, mail, passcode=None,
                 requested_user.passcode = None
                 # Mail already verified append key
                 if requested_user.is_mail_verified:
-                    requested_user.api_keys.append(new_key)
+                    requested_user.api_keys.append(ApiKey(user=requested_user, api_key=new_key))
                 # Else mark mail as verified and reset keys
                 else:
                     requested_user.is_mail_verified = True
-                    requested_user.api_keys = [new_key]
-                session.add(requested_user)
+                    requested_user.api_keys = [ApiKey(user=requested_user, api_key=new_key)]
+                #session.add(requested_user)
                 return user_with_key(requested_user, new_key)
             return helpers.response.error("Passcode authentication_failed", falcon.HTTP_401)
         # Exploration account
         elif not requested_user.is_mail_verified:
             # Ensure there is a key
             if not requested_user.api_keys:
-                requested_user.api_keys.append(new_key)
-                session.add(requested_user)
+                requested_user.api_keys.append(ApiKey(user=requested_user, api_key=new_key))
+                #session.add(requested_user)
+            else:
+                new_key = requested_user.api_keys[0].api_key
             return user_with_key(requested_user, new_key)
         # Everything failed
         return helpers.response.error("Auhentication required", falcon.HTTP_401)
@@ -67,7 +69,8 @@ def auth_by_mail(session: helpers.extend.session, response, mail, passcode=None,
 def send_passcode(session: helpers.extend.session, response, mail, test=False):
     """Send a verification mail """
     passcode = set_user_passcode(session, mail)
-    helpers.mail.from_template(mail, "verify_mail", body_params={"passcode": passcode}, test=test)
+    print(passcode)
+    #helpers.mail.from_template(mail, "verify_mail", body_params={"passcode": passcode}, test=test)
     return helpers.response.ok("mail_sent")
     
 @hug.post('/{mail}/set_passcode', requires=helpers.authentication.is_admin)
--- api/helpers/mail.py
+++ api/helpers/mail.py
@@ -7,7 +7,7 @@ MAIL_TEMPLATES = {
 		"body": """Bonjour
 
 Vous recevez ce courriel car vous avez demandé à sécuriser votre compte Mastok.
-Voici le code de sécurité à entrer dans l'application: {code}
+Voici le code de sécurité à entrer dans l'application: {passcode}
 
 A bientôt,
 Mastok
@@ -26,19 +26,19 @@ Subject: {mail_subject}
 def from_template(mail_to, name, subject_params={}, body_params={}, test=False):
     """Send Creates an account"""
     mail_from = config.get("smtp", "from")
-    mail_subject = MAIL_TEMPLATES[name]["subject"].format(subject_params)
-    mail_body = MAIL_TEMPLATES[name]["subject"].format(body_params)
+    mail_subject = MAIL_TEMPLATES[name]["subject"].format(**subject_params)
+    mail_body = MAIL_TEMPLATES[name]["body"].format(**body_params)
     mail_payload = MAIL_PAYLOAD_TEMPLATE.format(
         mail_from=mail_from,
         mail_to=mail_to,
         mail_subject=mail_subject,
         mail_body=mail_body)
-
+    
     # Send mail
     server = smtplib.SMTP_SSL(config.get("smtp", "host"), config.get("smtp", "port"))
     server.ehlo()
     server.login(config.get("smtp", "user"), config.get("smtp", "password"))
     if not test:
-    	server.sendmail(mail_from, mail_to, mail_payload.encode('utf-8'))
+        server.sendmail(mail_from, mail_to, mail_payload.encode('utf-8'))
     server.close()
     
